f4003e445867521abacb81b21fbed7d5cbbd4426
ZhangZhenghao <zhangzhenghao@hotmail.com>
2015-08-01 14:04:32 +0800

    Finifished level 2

---
+++ buflab/level2.d
@@ -0,0 +1,11 @@
+
+level2.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	68 9d 8c 04 08       	push   $0x8048c9d
+   5:	a1 08 d1 04 08       	mov    0x804d108,%eax
+   a:	a3 00 d1 04 08       	mov    %eax,0x804d100
+   f:	c3                   	ret    
---
+++ buflab/level2.s
@@ -0,0 +1,4 @@
+pushl	$0x08048c9d
+movl	0x804d108,%eax
+movl	%eax,0x804d100
+ret
