a69085b35a55a85bf71c660946873e8b3a9c6ecd
ZhangZhenghao <zhangzhenghao@hotmail.com>
2015-08-30 16:18:38 +0800

    Add macros

--- malloclab/mm.c
+++ malloclab/mm.c
@@ -44,11 +44,36 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+/* Basic constants and macros */
+#define WSIZE       4
+#define DSIZE       8
+#define CHUNKSIZE   (1<<12)
+
+/* Pack a size and allocated bit into a word */
+#define PACK(size, alloc)   ((size) | (alloc))
+
+/* Read and write a word at address p */
+#define GET(p)      (*(unsigned int*)(p))
+#define PUT(p, val) (*(unsigned int*)(p) = (val))
+
+/* Read the size and allocated fields from adress p */
+#define GET_SIZE(p)     (GET(p) & ~0x7)
+#define GET_ALLOC(p)    (GET(p) & 0x1)
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp)    ((char*)(bp) - WSIZE)
+#define FTRP(bp)    ((char*)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp)   ((char*)(bp) + GET_SIZE(((char*)(bp) - WSIZE)))
+#define PREV_BLKP(bp)   ((char*)(bp) - GET_SIZE(((char*)(bp) - DSIZE)))
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    /* Create the initial heap */
     return 0;
 }
 
