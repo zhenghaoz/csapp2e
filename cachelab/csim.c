f7b192cf43c0c9ddc55082472c34ad3c1d2dcbb6
ZhangZhenghao <zhangzhenghao@hotmail.com>
2015-09-01 18:25:17 +0800

    Parse address, build cache, free cache

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -5,11 +5,15 @@
 #include <string.h>
 #include <stdbool.h>
 
+/* Cache line struct */
 struct line {
 	bool valid;
 	unsigned long long tag;
 };
 
+/*
+ * printUsage - print usage
+ */
 static void printUsage(char const *exename)
 {
 	printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", exename);
@@ -25,14 +29,27 @@ static void printUsage(char const *exename)
 	printf("  linux>  %s -v -s 8 -E 2 -b 4 -t traces/yi.trace\n", exename);	
 }
 
+/* 
+ * parseAddr - parse 64-bit address
+ */
+static void parseAddr(unsigned long long addr, int s, int b, unsigned long long *tag, int *set, int *offset)
+{
+	*tag = addr >> (s+b);		/* Get tag */
+	*offset = addr & ~(-1<<b);	/* Get offset */
+	*set = addr>>b & ~(-1<<s);	/* Get set */
+}
+
 int main(int argc, char const *argv[])
 {
 	char instr;
 	unsigned long long address, size;
 	int miss = 0, hit = 0, eviction = 0;
-	int verbose = 0, setsize, lnsize, blksize;	/* Options */
+	int verbose = 0, setnum, lnnum, blksize;	/* Options */
 	char const *tracefile;	
 	FILE *fp;
+
+	/* Cache entry */
+	struct line **cache; 
 
 	/* Parse arguments */
 	int i;
@@ -43,13 +60,13 @@ int main(int argc, char const *argv[])
 		} else if (!strcmp(argv[i], "-v"))	/* Optional verbose flag */
 			verbose = 1;
 		else if (!strcmp(argv[i], "-s")) {	/* Number of set index bits */
-			if (!(setsize = atoi(argv[i+1]))) {
+			if (!(setnum = atoi(argv[i+1]))) {
 				printf("%s: Missing required command line argument\n", argv[0]);
 				printUsage(argv[0]);
 				return 1;
 			}
 		} else if (!strcmp(argv[i], "-E")) {/* Number of line per set */
-			if (!(lnsize = atoi(argv[i+1]))) {
+			if (!(lnnum = atoi(argv[i+1]))) {
 				printf("%s: Missing required command line argument\n", argv[0]);
 				printUsage(argv[0]);
 				return 1;
@@ -70,7 +87,7 @@ int main(int argc, char const *argv[])
 	}
 
 	/* Check options */
-	if (!setsize || !lnsize || !blksize) {
+	if (!setnum || !lnnum || !blksize) {
 		printf("%s: option requires an argument -- 't'\n", argv[0]);
 		printUsage(argv[0]);
 		return 1;
@@ -83,21 +100,36 @@ int main(int argc, char const *argv[])
 	}
 
 	/* Build cache */
-
+	cache = (struct line**) malloc(setnum*sizeof(struct line*)); 
+	for (int i = 0; i < setnum; ++i) {
+		cache[i] = (struct line*) malloc(lnnum*sizeof(struct line));
+		/* Initial cache */
+		for (int j = 0; j < lnnum; ++j)
+			cache[i][j].valid = 0;
+	}
 
 	/* Load instrument */
 	while (fscanf(fp, " %c %llu,%llu", &instr, &address, &size) != EOF) {
 		if (verbose)
 			printf("%c %llu,%llu\n", instr, address, size);
+		/* Parse address */
+		unsigned long long tag;
+		int set, offset;
+		parseAddr(address, setnum, blksize, &tag, &set, &offset);
 		switch (instr) {
-			case 'M':
+			case 'M':	/* Data modify */
 				break;
-			case 'L':
+			case 'L':	/* Data load */
 				break;
-			case 'S':
+			case 'S':	/* Data store */
 				break;
 		}
 	}
+
+	/* Free cache */
+	for (int i = 0; i < setnum; ++i)
+		free(cache[i]);
+	free(cache);
 
 	/* Close file */
 	fclose(fp);
