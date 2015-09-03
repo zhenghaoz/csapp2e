3fb5c63ede495450f28d5860db3279bf5da0f8df
ZhangZhenghao <zhangzhenghao@hotmail.com>
2015-09-03 16:23:11 +0800

    FInished csim

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -2,29 +2,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <limits.h>
 
 /* Parse address */
 #define GET_TAG(addr)		(addr >> (s+b))
-#define GET_OFFSET(addr)	(addr & ~(-1L<<b))
-#define GET_SET(addr)		((addr>>b) & ~(-1L<<s))
+#define GET_SET(addr)		((addr>>b) & ~(ULLONG_MAX<<s))
+
+/* Constant */
+#define SET_COUNT	(1<<s)
 
 /* Cache line struct */
 struct line {
 	bool valid;
 	unsigned long long tag;
+	unsigned long long lru_value;
 };
 
-/* Varinet */
+/* Global varient  */
 static int hit_count = 0, miss_count = 0, eviction_count = 0;	/* Summary */
-static int s, b, E;											/* Option */
-static struct line **cache;									/* Cache entry */
+static int s, b, E;												/* Option */
+static int verbose = 0;											/* Verbose */
+static char const *tracefile;									/* Trace file */
+static struct line **cache;										/* Cache entry */
+
+/* Extern function */
+int getopt(int argc, char const *argv[], const char *optstring);
 
 /*
  * printUsage - print usage
  */
-static void printUsage(char const *exename)
+static void print_usage(char const *exename)
 {
 	printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", exename);
 	printf("Options:\n");
@@ -42,107 +51,133 @@ static void printUsage(char const *exename)
 /*
  * load - load data
  */
-static void load(unsigned long long addr)
+static void cache_access(unsigned long long addr)
 {
-
-}
-
-/*
- * save - save data
- */
-static void save(unsigned long long addr)
-{
-
-}
-
-/*
- * modify - modify data
- */
-static void modify(unsigned long long addr)
-{
-
+	unsigned long long tag = GET_TAG(addr);	/* Get tag */
+	int set = GET_SET(addr);				/* Get set index */
+	struct line* lines = cache[set];		/* Get set entry */
+	unsigned long long min = ULLONG_MAX;	
+	int aim = -1, first_empty = -1, lru = -1;
+	for (int i = 0; i < E; ++i) {
+		/* Find hit line */
+		if (lines[i].valid && lines[i].tag == tag)
+			aim = i;
+		/* Decrease LRU value */
+		if (lines[i].valid)
+			lines[i].lru_value--;
+		/* Find unvaliable line */
+		if (!lines[i].valid && first_empty == -1)
+			first_empty = i;
+		/* Find LRU line */
+		if (lines[i].valid && lines[i].lru_value <= min) {
+			lru = i;
+			min = lines[i].lru_value;
+		}
+	}
+	/* Hit */
+	if (aim != -1) {
+		lines[aim].lru_value = ULLONG_MAX;
+		hit_count++;
+		if (verbose)
+			printf(" hit");
+		return;
+	}
+	/* Miss */
+	miss_count++;
+	if (first_empty != -1) {
+		lines[first_empty].valid = 1;
+		lines[first_empty].tag = tag;
+		lines[first_empty].lru_value = ULLONG_MAX;
+		if (verbose)
+			printf(" miss");
+		return;
+	}
+	/* Eviction */
+	lines[lru].valid = 1;
+	lines[lru].tag = tag;
+	lines[lru].lru_value = ULLONG_MAX;
+	eviction_count++;
+	if (verbose)
+		printf(" miss eviction");
 }
 
 int main(int argc, char const *argv[])
 {
-	char instr;
-	unsigned long long address, size;
-	char const *tracefile;	
-	FILE *fp;
-
 	/* Parse arguments */
-	int i;
-	for (i = 1; i < argc; ++i) {
-		if (!strcmp(argv[i], "-h")) {	/* Help */
-			printUsage(argv[0]);
-			return 0;
-		} else if (!strcmp(argv[i], "-v"))	/* Optional verbose flag */
-			verbose = 1;
-		else if (!strcmp(argv[i], "-s")) {	/* Number of set index bits */
-			if (!(setnum = atoi(argv[i+1]))) {
-				printf("%s: Missing required command line argument\n", argv[0]);
-				printUsage(argv[0]);
+	int result;
+	extern char *optarg;
+	while ((result = getopt(argc, argv, "hvs:E:b:t:")) != EOF) {
+		switch (result) {
+			case 'h':	/* Optional help flag that print usage info */
+				print_usage(argv[0]);
+				return 0;
+			case 'v':	/* Optional verbose flag that display trace info */
+				verbose = 1;
+				break;
+			case 's':	/* Number of set index bits */
+				s = atoi(optarg);
+				break;
+			case 'E':	/* Associativity (number of line per set) */
+				E = atoi(optarg);
+				break;
+			case 'b':	/* Number of block bits */
+				b = atoi(optarg);
+				break;
+			case 't':	/* Name of trace file */
+				tracefile = optarg;
+				break;
+			case '?':	/* Get argumnets failed */
+				print_usage(argv[0]);
 				return 1;
-			}
-		} else if (!strcmp(argv[i], "-E")) {/* Number of line per set */
-			if (!(lnnum = atoi(argv[i+1]))) {
-				printf("%s: Missing required command line argument\n", argv[0]);
-				printUsage(argv[0]);
-				return 1;
-			}
-		} else if (!strcmp(argv[i], "-b")) {/* Number of block offset bits */
-			if (!(blksize = atoi(argv[i+1]))) {
-				printf("%s: Missing required command line argument\n", argv[0]);
-				printUsage(argv[0]);
-				return 1;
-			}
-		} else if (!strcmp(argv[i], "-t")) {/* Trace file */
-			if ((tracefile = argv[++i]) == NULL) {
-				printf("%s: option requires an argument -- 't'\n", argv[0]);
-				printUsage(argv[0]);
-				return 1;
-			}
 		}
 	}
 
 	/* Check options */
-	if (!setnum || !lnnum || !blksize) {
-		printf("%s: option requires an argument -- 't'\n", argv[0]);
-		printUsage(argv[0]);
+	if (!s || !E || !b) {
+		printf("%s: Missing required command line argument\n", argv[0]);
+		print_usage(argv[0]);
 		return 1;
-	}
+	} 
 
 	/* Open trace file */
+	FILE *fp;
 	if ((fp = fopen(tracefile, "r")) == NULL) {
 		printf("%s: No such file or directory\n", tracefile);
 		return 1;
 	}
 
 	/* Build cache */
-	cache = (struct line**) malloc(setnum*sizeof(struct line*)); 
-	for (int i = 0; i < setnum; ++i) {
-		cache[i] = (struct line*) malloc(lnnum*sizeof(struct line));
+	cache = (struct line**) malloc(SET_COUNT*sizeof(struct line*)); 
+	for (int i = 0; i < SET_COUNT; ++i) {
+		cache[i] = (struct line*) malloc(E*sizeof(struct line));
 		/* Initial cache */
-		for (int j = 0; j < lnnum; ++j)
+		for (int j = 0; j < E; ++j) {
 			cache[i][j].valid = 0;
-	}
-
-	/* Load instrument */
-	while (fscanf(fp, " %c %llu,%llu", &instr, &address, &size) != EOF) {
-		if (verbose)
-			printf("%c %llu,%llu\n", instr, address, size);
-		switch (instr) {
-			case 'M':	/* Data modify */
-				break;
-			case 'L':	/* Data load */
-				break;
-			case 'S':	/* Data store */
-				break;
+			cache[i][j].tag = ULLONG_MAX;
 		}
 	}
 
+	/* Load instrument */
+	char instr;
+	unsigned long long addr;
+	int size;
+	while (fscanf(fp, " %c %llx,%d", &instr, &addr, &size) != EOF) {
+		if (verbose)
+			printf("%c %llx,%d", instr, addr, size);
+		switch (instr) {
+			case 'M':	/* Data modify */
+				cache_access(addr);
+			case 'L':	/* Data load */
+			case 'S':	/* Data store */
+				cache_access(addr);
+				break;
+		}
+		if (verbose)
+			printf("\n");
+	}
+
 	/* Free cache */
-	for (int i = 0; i < setnum; ++i)
+	for (int i = 0; i < SET_COUNT; ++i)
 		free(cache[i]);
 	free(cache);
 
