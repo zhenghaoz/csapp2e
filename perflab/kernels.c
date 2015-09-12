80721bf3d28637ff60d731c4e89598f1a40dd949
ZhangZhenghao <zhangzhenghao@hotmail.com>
2015-09-12 16:00:50 +0800

    Optimezed rotate function

--- perflab/kernels.c
+++ perflab/kernels.c
@@ -10,10 +10,10 @@
  * Please fill in the following team struct 
  */
 team_t team = {
-    "bovik",              /* Team name */
+    "team-z",              /* Team name */
 
-    "Harry Q. Bovik",     /* First member full name */
-    "bovik@nowhere.edu",  /* First member email address */
+    "Zhang Zhenghao",     /* First member full name */
+    "zhangzhenghao@zjut.edu.cn",  /* First member email address */
 
     "",                   /* Second member full name (leave blank if none) */
     ""                    /* Second member email addr (leave blank if none) */
@@ -47,7 +47,85 @@ void naive_rotate(int dim, pixel *src, pixel *dst)
 char rotate_descr[] = "rotate: Current working version";
 void rotate(int dim, pixel *src, pixel *dst) 
 {
-    naive_rotate(dim, src, dst);
+    int i, j;
+
+    for (i = 0; i < dim; i+=32)
+    for (j = 0; j < dim; j++) {
+        dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+        dst[RIDX(dim-1-j, i+1, dim)] = src[RIDX(i+1, j, dim)];
+        dst[RIDX(dim-1-j, i+2, dim)] = src[RIDX(i+2, j, dim)];
+        dst[RIDX(dim-1-j, i+3, dim)] = src[RIDX(i+3, j, dim)];
+        dst[RIDX(dim-1-j, i+4, dim)] = src[RIDX(i+4, j, dim)];
+        dst[RIDX(dim-1-j, i+5, dim)] = src[RIDX(i+5, j, dim)];
+        dst[RIDX(dim-1-j, i+6, dim)] = src[RIDX(i+6, j, dim)];
+        dst[RIDX(dim-1-j, i+7, dim)] = src[RIDX(i+7, j, dim)];
+        dst[RIDX(dim-1-j, i+8, dim)] = src[RIDX(i+8, j, dim)];
+        dst[RIDX(dim-1-j, i+9, dim)] = src[RIDX(i+9, j, dim)];
+        dst[RIDX(dim-1-j, i+10, dim)] = src[RIDX(i+10, j, dim)];
+        dst[RIDX(dim-1-j, i+11, dim)] = src[RIDX(i+11, j, dim)];
+        dst[RIDX(dim-1-j, i+12, dim)] = src[RIDX(i+12, j, dim)];
+        dst[RIDX(dim-1-j, i+13, dim)] = src[RIDX(i+13, j, dim)];
+        dst[RIDX(dim-1-j, i+14, dim)] = src[RIDX(i+14, j, dim)];
+        dst[RIDX(dim-1-j, i+15, dim)] = src[RIDX(i+15, j, dim)];
+        dst[RIDX(dim-1-j, i+16, dim)] = src[RIDX(i+16, j, dim)];
+        dst[RIDX(dim-1-j, i+17, dim)] = src[RIDX(i+17, j, dim)];
+        dst[RIDX(dim-1-j, i+18, dim)] = src[RIDX(i+18, j, dim)];
+        dst[RIDX(dim-1-j, i+19, dim)] = src[RIDX(i+19, j, dim)];
+        dst[RIDX(dim-1-j, i+20, dim)] = src[RIDX(i+20, j, dim)];
+        dst[RIDX(dim-1-j, i+21, dim)] = src[RIDX(i+21, j, dim)];
+        dst[RIDX(dim-1-j, i+22, dim)] = src[RIDX(i+22, j, dim)];
+        dst[RIDX(dim-1-j, i+23, dim)] = src[RIDX(i+23, j, dim)];
+        dst[RIDX(dim-1-j, i+24, dim)] = src[RIDX(i+24, j, dim)];
+        dst[RIDX(dim-1-j, i+25, dim)] = src[RIDX(i+25, j, dim)];
+        dst[RIDX(dim-1-j, i+26, dim)] = src[RIDX(i+26, j, dim)];
+        dst[RIDX(dim-1-j, i+27, dim)] = src[RIDX(i+27, j, dim)];
+        dst[RIDX(dim-1-j, i+28, dim)] = src[RIDX(i+28, j, dim)];
+        dst[RIDX(dim-1-j, i+29, dim)] = src[RIDX(i+29, j, dim)];
+        dst[RIDX(dim-1-j, i+30, dim)] = src[RIDX(i+30, j, dim)];
+        dst[RIDX(dim-1-j, i+31, dim)] = src[RIDX(i+31, j, dim)];
+    }
+}
+
+char rotate_descr_sr[] = "rotate: Succesive reading version";
+void rotate_sr(int dim, pixel *src, pixel *dst) 
+{
+    int i, j;
+
+    for (i = 0; i < dim; i++)
+    for (j = 0; j < dim; j+=32) {
+        dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+        dst[RIDX(dim-2-j, i, dim)] = src[RIDX(i, j+1, dim)];
+        dst[RIDX(dim-3-j, i, dim)] = src[RIDX(i, j+2, dim)];
+        dst[RIDX(dim-4-j, i, dim)] = src[RIDX(i, j+3, dim)];
+        dst[RIDX(dim-5-j, i, dim)] = src[RIDX(i, j+4, dim)];
+        dst[RIDX(dim-6-j, i, dim)] = src[RIDX(i, j+5, dim)];
+        dst[RIDX(dim-7-j, i, dim)] = src[RIDX(i, j+6, dim)];
+        dst[RIDX(dim-8-j, i, dim)] = src[RIDX(i, j+7, dim)];
+        dst[RIDX(dim-9-j, i, dim)] = src[RIDX(i, j+8, dim)];
+        dst[RIDX(dim-10-j, i, dim)] = src[RIDX(i, j+9, dim)];
+        dst[RIDX(dim-11-j, i, dim)] = src[RIDX(i, j+10, dim)];
+        dst[RIDX(dim-12-j, i, dim)] = src[RIDX(i, j+11, dim)];
+        dst[RIDX(dim-13-j, i, dim)] = src[RIDX(i, j+12, dim)];
+        dst[RIDX(dim-14-j, i, dim)] = src[RIDX(i, j+13, dim)];
+        dst[RIDX(dim-15-j, i, dim)] = src[RIDX(i, j+14, dim)];
+        dst[RIDX(dim-16-j, i, dim)] = src[RIDX(i, j+15, dim)];
+        dst[RIDX(dim-17-j, i, dim)] = src[RIDX(i, j+16, dim)];
+        dst[RIDX(dim-18-j, i, dim)] = src[RIDX(i, j+17, dim)];
+        dst[RIDX(dim-19-j, i, dim)] = src[RIDX(i, j+18, dim)];
+        dst[RIDX(dim-20-j, i, dim)] = src[RIDX(i, j+19, dim)];
+        dst[RIDX(dim-21-j, i, dim)] = src[RIDX(i, j+20, dim)];
+        dst[RIDX(dim-22-j, i, dim)] = src[RIDX(i, j+21, dim)];
+        dst[RIDX(dim-23-j, i, dim)] = src[RIDX(i, j+22, dim)];
+        dst[RIDX(dim-24-j, i, dim)] = src[RIDX(i, j+23, dim)];
+        dst[RIDX(dim-25-j, i, dim)] = src[RIDX(i, j+24, dim)];
+        dst[RIDX(dim-26-j, i, dim)] = src[RIDX(i, j+25, dim)];
+        dst[RIDX(dim-27-j, i, dim)] = src[RIDX(i, j+26, dim)];
+        dst[RIDX(dim-28-j, i, dim)] = src[RIDX(i, j+27, dim)];
+        dst[RIDX(dim-29-j, i, dim)] = src[RIDX(i, j+28, dim)];
+        dst[RIDX(dim-30-j, i, dim)] = src[RIDX(i, j+29, dim)];
+        dst[RIDX(dim-31-j, i, dim)] = src[RIDX(i, j+30, dim)];
+        dst[RIDX(dim-32-j, i, dim)] = src[RIDX(i, j+31, dim)];
+    }
 }
 
 /*********************************************************************
@@ -61,7 +139,8 @@ void rotate(int dim, pixel *src, pixel *dst)
 void register_rotate_functions() 
 {
     add_rotate_function(&naive_rotate, naive_rotate_descr);   
-    add_rotate_function(&rotate, rotate_descr);   
+    add_rotate_function(&rotate, rotate_descr);  
+    add_rotate_function(&rotate_sr, rotate_descr_sr);   
     /* ... Register additional test functions here */
 }
 
@@ -176,8 +255,8 @@ void smooth(int dim, pixel *src, pixel *dst)
  *********************************************************************/
 
 void register_smooth_functions() {
-    add_smooth_function(&smooth, smooth_descr);
     add_smooth_function(&naive_smooth, naive_smooth_descr);
+    add_smooth_function(&smooth, smooth_descr);
     /* ... Register additional test functions here */
 }
 
